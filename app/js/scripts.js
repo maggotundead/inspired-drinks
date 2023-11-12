cd09adbad026cadbc511cc2b4bba39ad50cb1cc1
Roman Salko <maggotundead@yandex.com>
2023-11-12 17:35:04 +0200

    beta v

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -31,3 +31,72 @@ if (blockProduct) {
         },
     });
 }
+
+const blockSustainable = document.querySelector('.block-sustainable');
+if (blockSustainable) {
+  function Marquee(selector, speed) {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    // console.log(firstElement);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+
+    setInterval(function () {
+      firstElement.style.marginLeft = `-${i}px`;
+      if (i > firstElement.clientWidth) {
+        i = 0;
+      }
+      i = i + speed;
+    }, 0);
+  }
+
+  window.addEventListener('load', Marquee('.marquee', 0.8));
+}
+
+
+const blockMinerals = document.querySelector('.block-water-minerals');
+if (blockMinerals) {
+  const scrollableWrapper = blockMinerals.querySelector('.scrollable-wrapper');
+
+  let blockOffsetTop = blockMinerals.offsetTop;
+  let visualInitialOffset = window.outerHeight / 2;
+  let visualOffset = window.outerHeight / 4;
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', function() {
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+
+    // console.log(`blockOffsetTop ${blockOffsetTop}, st ${st}, visualOffset ${ visualOffset}`);
+
+    if (st > lastScrollTop) {
+      // console.log('down');
+
+      if ( st > blockOffsetTop - visualInitialOffset ) {
+        !scrollableWrapper.classList.contains('stage-1') && scrollableWrapper.classList.add('stage-1');
+      }
+      if ( st > blockOffsetTop - visualOffset) {
+        !scrollableWrapper.classList.contains('stage-2') && scrollableWrapper.classList.add('stage-2');
+      }
+      if ( st > blockOffsetTop ) {
+        !scrollableWrapper.classList.contains('stage-3') && scrollableWrapper.classList.add('stage-3');
+      }
+    }
+    // else if (st < lastScrollTop) {
+    //   // console.log('up');
+
+    //   if ( st < blockOffsetTop - visualInitialOffset  ) {
+    //     scrollableWrapper.classList.contains('stage-1') && scrollableWrapper.classList.remove('stage-1');
+    //   }
+    //   if ( st < blockOffsetTop - visualOffset ) {
+    //     scrollableWrapper.classList.contains('stage-2') && scrollableWrapper.classList.remove('stage-2');
+    //   }
+    //   if ( st < blockOffsetTop ) {
+    //     scrollableWrapper.classList.contains('stage-3') && scrollableWrapper.classList.remove('stage-3');
+    //   }
+    // }
+
+    lastScrollTop = st <= 0 ? 0 : st;
+  }, false);
+}
