f11df60d3e204ce3d04e2d19b6ac96203f436039
Roman Salko <maggotundead@yandex.com>
2023-11-21 19:41:36 +0200

    update

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -113,3 +113,70 @@ if (blockMinerals) {
     lastScrollTop = st <= 0 ? 0 : st;
   }, false);
 }
+
+// Cookies
+
+// Create cookie
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = 'expires='+ d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
+
+// Delete cookie
+function deleteCookie(cname) {
+  const d = new Date();
+  d.setTime(d.getTime() + (24*60*60*1000));
+  let expires = 'expires='+ d.toUTCString();
+  document.cookie = cname + '=;' + expires + ';path=/';
+}
+
+// Read cookie
+function getCookie(cname) {
+  let name = cname + '=';
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return '';
+}
+
+// Set cookie consent
+function acceptCookieConsent() {
+  deleteCookie('user_cookie_consent');
+  setCookie('user_cookie_consent', 1, 30);
+  cookieModal.classList.remove('active');
+}
+
+// modals
+let cookie_consent = getCookie('user_cookie_consent');
+const cookieModal = document.getElementById('cookieNotice');
+
+if (cookie_consent != '') {
+  cookieModal.classList.remove('active');
+} else {
+  cookieModal.classList.add('active');
+}
+
+document.querySelectorAll('.modal-close').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.modal').forEach(modal => {
+      modal.classList.remove('active');
+    });
+  });
+});
+document.querySelectorAll('.modal-overlay').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.modal').forEach(modal => {
+      modal.classList.remove('active');
+    });
+  });
+});
